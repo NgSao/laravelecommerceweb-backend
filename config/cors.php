7ac1f70fa17caa13437ef3d4a0c78b4d63294492
NgSao <nguyensaovn2019@gmail.com>
2025-06-18 14:57:57 +0700

    bat cros

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://laravelecommerceweb-frontend.vercel.app'],
+    'allowed_origins' => ['https://laravelecommerceweb-frontend.vercel.app','http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
